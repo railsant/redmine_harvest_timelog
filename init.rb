cbe102cc8cee966d92c65e7345624f0bb5bd7dba
Family <family@apple-computer.local>
2011-01-30 22:48:31 +0800

    Support Account Setting with Redmine Configuration

--- init.rb
+++ init.rb
@@ -1,6 +1,13 @@
 require 'redmine'
-require 'harvested'
 require 'dispatcher'
+
+['harvested'].each do |gem|
+  begin
+    require gem
+  rescue LoadError
+    raise "You are missing the '#{gem}' gem"
+  end
+end
 
 Dispatcher.to_prepare do
   require_dependency 'time_entry'
@@ -9,6 +16,9 @@ Dispatcher.to_prepare do
     harvest_user_id_custom_id = Setting.plugin_redmine_harvest_timelog['harvest_user_id']
     harvest_project_id_custom_id = Setting.plugin_redmine_harvest_timelog['harvest_project_id']
     harvest_task_id_custom_id = Setting.plugin_redmine_harvest_timelog['harvest_task_id']
+    harvest_domain = Setting.plugin_redmine_harvest_timelog['harvest_domain']
+    harvest_email = Setting.plugin_redmine_harvest_timelog['harvest_email']
+    harvest_password = Setting.plugin_redmine_harvest_timelog['harvest_password']
     
     # harvest user id 
     custom_value = time_entry.user.custom_values.detect {|v| v.custom_field_id == harvest_user_id_custom_id.to_i}
@@ -24,18 +34,19 @@ Dispatcher.to_prepare do
 
     if harvest_user_id && harvest_project_id && harvest_task_id
       time = Harvest::TimeEntry.new(:notes => time_entry.comments, :hours => time_entry.hours, :project_id => harvest_project_id, :task_id => harvest_task_id, :of_user => harvest_user_id)
-      config = YAML::load(File.read(RAILS_ROOT + "/config/harvest.yml"))
-      harvest = Harvest.hardy_client(config["domain"], config["email"], config["password"])
+      harvest = Harvest.hardy_client(harvest_domain, harvest_email, harvest_password)
       harvest.time.create(time) 
     end rescue nil
   end
 end
 
+redmine_harvest_timelog_config = YAML::load(File.read(RAILS_ROOT + "/config/harvest.yml"))
+
 Redmine::Plugin.register :redmine_harvest_timelog do
   name 'Redmine Harvest Time log plugin'
   author 'Benjamin Wong'
   description 'This is a plugin for Redmine to export project timelog data to Harvest.'
-  version '0.0.1'
+  version '0.0.2'
   # url 'http://example.com/path/to/plugin'
   # author_url 'http://example.com/about'
   
@@ -43,7 +54,10 @@ Redmine::Plugin.register :redmine_harvest_timelog do
   settings :default => {
     'harvest_project_id' => '',
     'harvest_user_id' => '', 
-    'harvest_task_id' => ''
+    'harvest_task_id' => '', 
+    'harvest_domain' => redmine_harvest_timelog_config["domain"], 
+    'harvest_email' => redmine_harvest_timelog_config["email"], 
+    'harvest_password' => redmine_harvest_timelog_config["password"], 
   }, :partial => 'settings/harvest_settings'
   
 end
